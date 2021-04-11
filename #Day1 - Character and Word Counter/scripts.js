959679775bdec2c683de55e53af437fcb867012c
Programming-Inspire <67471717+Programming-inspire@users.noreply.github.com>
2021-04-11 10:06:59 +0530

    Add files via upload

---
+++ #Day1 - Character and Word Counter/scripts.js
@@ -0,0 +1,12 @@
+const calculate = () =>{
+    let input = document.getElementById('input').value;
+
+    let remText = input.replace(/\s/g, "")
+    let noc = remText.length;
+    let output1 = document.getElementById('output1').innerHTML="Total Characters = " + noc;
+
+
+    let count = input.match(/\w+/g);
+    let res = count.length;
+    let output2 = document.getElementById('output2').innerHTML="Total Worsds =" + res;
+}
