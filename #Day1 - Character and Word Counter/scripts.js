00924bfe0872f0ad6baaaf3000807353eaad2dcd
Programming-Inspire <67471717+Programming-inspire@users.noreply.github.com>
2021-04-11 10:09:07 +0530

    Update scripts.js

--- #Day1 - Character and Word Counter/scripts.js
+++ #Day1 - Character and Word Counter/scripts.js
@@ -8,5 +8,5 @@ const calculate = () =>{
 
     let count = input.match(/\w+/g);
     let res = count.length;
-    let output2 = document.getElementById('output2').innerHTML="Total Worsds =" + res;
+    let output2 = document.getElementById('output2').innerHTML="Total Words =" + res;
 }
