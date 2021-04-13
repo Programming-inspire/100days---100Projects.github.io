7a4d98269902d34117335041f7edce481ce692bf
Programming-Inspire <67471717+Programming-inspire@users.noreply.github.com>
2021-04-13 11:42:31 +0530

    Add files via upload

---
+++ #Day3 - TO-DO App/script.js
@@ -0,0 +1,26 @@
+$(".txtb").on("keyup", function(e){
+    if(e.keyCode == 13 && $(".txtb").val() != "")
+    {
+        var task = $("<div class='task'></div>").text($(".txtb").val());
+        var del = $("<i class='fas fa-trash-alt'></i>").click(function(){
+            var p = $(this).parent();
+            p.fadeOut(function(){
+                p.remove();
+            })
+        });
+
+        var check = $("<i class='fas fa-check'></i>").click(function(){
+            var p = $(this).parent();
+            p.fadeOut(function(){
+                $('.comp').append(p);
+                p.fadeIn();
+            });
+            $(this).remove();
+        });
+
+        task.append(del, check);
+        $(".notcomp").append(task);
+        //clear input
+        $(".txtb").val("");
+    }
+})
