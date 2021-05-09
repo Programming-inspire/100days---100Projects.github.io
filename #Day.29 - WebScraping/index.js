71b32a97a9160e98aa37408c45730147c7229d67
Programming-Inspire <67471717+Programming-inspire@users.noreply.github.com>
2021-05-09 15:57:58 +0530

    Add files via upload

---
+++ #Day.29 - WebScraping/index.js
@@ -0,0 +1,35 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+let table = [];
+
+const url = 'https://www.imdb.com/chart/top/'
+request(url, function(err, res, data){
+    if(!err)
+    {
+        const $ = cheerio.load(data)
+        const elementSelector = '#main > div > span > div > div > div.lister > table > tbody > tr '
+    
+        $(elementSelector).each(function(id) {
+            if(id <= 40)
+            {
+            var MovieName= $(this).find(".titleColumn > a").html();  
+            var year = $(this).find(".titleColumn > span").html();
+            var rating = $(this).find(".imdbRating > strong").html();
+            table.push(
+                {
+                    'MovieName': MovieName, 'Year of Release': year, 'Rating': rating 
+                }
+            )
+            }
+         });
+         console.table(table);
+         
+    }
+    else{
+        console.log(err);
+    }
+
+})
+      
+    
