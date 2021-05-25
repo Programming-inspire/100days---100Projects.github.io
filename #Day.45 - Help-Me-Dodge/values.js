af8b2dcf1d064295c4061a0378742d36338fd16e
Programming-Inspire <67471717+Programming-inspire@users.noreply.github.com>
2021-05-25 15:42:40 +0530

    Add files via upload

---
+++ #Day.45 - Help-Me-Dodge/values.js
@@ -0,0 +1,15 @@
+var PipeColor = "green";
+var PlayerColor = "blue";
+
+var BirdSpeed = 3;
+var PipeSpeed = 1;
+var PipeGap = 50;
+
+var UpKey = "ArrowLeft";
+var DownKey = "ArrowRight";
+
+var Message = "DEAD!  Play again";
+var GameName = "Help Me DODGE ";
+var DisplayScore = true;
+
+
