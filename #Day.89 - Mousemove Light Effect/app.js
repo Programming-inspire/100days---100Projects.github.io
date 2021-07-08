a55cdf0bb1b5f796215e8ae7c282040289686a3a
Programming-Inspire <67471717+Programming-inspire@users.noreply.github.com>
2021-07-08 19:02:39 +0530

    Add files via upload

---
+++ #Day.89 - Mousemove Light Effect/app.js
@@ -0,0 +1,7 @@
+var mouseMovement = document.documentElement;
+
+
+mouseMovement.addEventListener("mousemove", (e) => {
+  mouseMovement.style.setProperty("--x", e.clientX + "px");
+  mouseMovement.style.setProperty("--y", e.clientY + "px");
+});
