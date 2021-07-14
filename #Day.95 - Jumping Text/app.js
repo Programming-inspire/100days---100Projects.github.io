b89f3f33bb527c867980d96c55595ab837e869d7
Programming-Inspire <67471717+Programming-inspire@users.noreply.github.com>
2021-07-14 19:12:38 +0530

    Add files via upload

---
+++ #Day.95 - Jumping Text/app.js
@@ -0,0 +1,13 @@
+const letters = document.querySelectorAll(".letters span");
+
+
+letters.forEach((letter, index) => {
+  letter.addEventListener("click", (e) => {
+    e.target.classList.add("active");
+  });
+  letter.addEventListener("animationend", (e) => {
+    e.target.classList.remove("active");
+  });
+
+
+});
