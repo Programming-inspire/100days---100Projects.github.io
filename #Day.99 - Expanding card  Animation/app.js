ea520d79b82e60a767fcc723c5a9897315896145
Programming-Inspire <67471717+Programming-inspire@users.noreply.github.com>
2021-07-18 18:36:59 +0530

    Add files via upload

---
+++ #Day.99 - Expanding card  Animation/app.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(panel => {
+    panel.addEventListener('click', ()=> {
+        removingActiveClass();
+        panel.classList.add('active');
+    })
+})
+
+function removingActiveClass() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
+
